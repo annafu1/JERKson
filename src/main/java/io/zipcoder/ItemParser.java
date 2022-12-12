5e572acaa1d81a7021a3664832b7b06bea095cb7
annafu1 <104175541+annafu1@users.noreply.github.com>
2022-12-12 15:45:43 -0500

    updated

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,15 +3,48 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.List;
 
 public class ItemParser {
     public List<Item> parseItemList(String valueToParse) {
-
-        return null;
+        try {
+            String[] items = valueToParse.split("##");
+            ArrayList<Item> list = new ArrayList<>();
+            for (String t : items) {
+               // list.add(parseSingleItem(t));
+            }
+            return list;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+        // Pattern pattern = Pattern.compile("milk"
+//        Matcher matcher = pattern.matcher(valueToParse);
+//        //String[] input = valueToParse.split(",\\s*");
+//        List<Item> list = new ArrayList<>();
+//        while (matcher.find()) {
+//            list.add(matcher.group());
+//        };
+//        String first = mylist.get(valueToParse);
+    //return first;
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+//        Matcher m = Pattern.compile("(\\b\\B)").matcher(singleItem);
+//        while(m.find()) {
+//            m.group();
+//        }
+//        return m;
+//    } throw new (ItemParseException());
+
+        String[] props = singleItem.replace(";", " ").split(" ");
+        for (String t : props) {
+            t.replace("(\\b\\B)", " ");
+        }
+        throw (new ItemParseException());
     }
 }

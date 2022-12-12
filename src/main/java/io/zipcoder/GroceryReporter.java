4e757b441cb74f97e009317df2003487a63e804e
annafu1 <104175541+annafu1@users.noreply.github.com>
2022-12-12 12:12:06 -0500

    updated

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -10,27 +10,15 @@ public class GroceryReporter {
     private final String originalFileText;
 
     public GroceryReporter(String jerksonFileName) {
-        try {
            this.originalFileText = FileReader.readFile(jerksonFileName);
-//            try {
-//                Matcher m = Pattern.compile("[Milk]").matcher(jerksonFileName);
-//                while (m.find()) {
-//                    System.out.println(m.group());
-//                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-//            }
-//        } catch (RuntimeException e) {
-//            throw new RuntimeException(e);
-        }
     }
 
     @Override
     public String toString() {
-        String s = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##\n";
-        StringBuilder sb = new StringBuilder(s)
-                .insert(0,"name:" + " \t\t\t\t " + "seen: " + "times\n" + "=============        =============\n" + "Price: " + " \t\t\t " + "seen: " + "times\n");
-                String output = sb.toString();
+        //String s = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##\n";
+        StringBuilder sb = new StringBuilder(originalFileText)
+                .insert(0,"name:" + " \t\t\t\t " + "seen: " + "times\n" + "=============        =============\n" + "Price: " + " \t\t\t " + "seen: " + "times\n" + "-------------        -------------\n" + "Price: " + " \t\t\t " + "seen: " + "times\n");
+        String output = sb.toString();
         return output;
 //        String ss[] = s.split("\\s");
 //        for (int i = 0; i <ss.length; i++) {
@@ -38,4 +26,10 @@ public class GroceryReporter {
 //        }
 //        return ;
     }
+
+//    public String JsonToString () {
+//        JsonParser parser = new JsonParser();
+//        JsonObject JSONObject = parser.parse(s).getAsJsonJect();
+//        return JSONObject;
+//    }
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class ItemParser {
     public List<Item> parseItemList(String valueToParse) {
+
         return null;
     }
 

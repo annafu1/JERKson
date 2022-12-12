3c2d98af6aab2f138f15c70c28f918dcfcb9f9f2
annafu1 <104175541+annafu1@users.noreply.github.com>
2022-12-12 17:29:13 -0500

    updated

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -2,10 +2,9 @@ package io.zipcoder;
 
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
+import io.zipcoder.utils.match.MatchGroup;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.List;
@@ -16,14 +15,14 @@ public class ItemParser {
             String[] items = valueToParse.split("##");
             ArrayList<Item> list = new ArrayList<>();
             for (String t : items) {
-               // list.add(parseSingleItem(t));
+               list.add(parseSingleItem(t));
             }
             return list;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-        // Pattern pattern = Pattern.compile("milk"
+        // Pattern pattern = Pattern.compile("milk")
 //        Matcher matcher = pattern.matcher(valueToParse);
 //        //String[] input = valueToParse.split(",\\s*");
 //        List<Item> list = new ArrayList<>();
@@ -34,17 +33,21 @@ public class ItemParser {
     //return first;
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-//        Matcher m = Pattern.compile("(\\b\\B)").matcher(singleItem);
-//        while(m.find()) {
-//            m.group();
-//        }
-//        return m;
-//    } throw new (ItemParseException());
+        Pattern pattern = Pattern.compile("\\w");
+        Matcher m = pattern.matcher(singleItem);
+        MatchGroup mg = new MatchGroup(m);
+        while(m.find()) {
 
-        String[] props = singleItem.replace(";", " ").split(" ");
-        for (String t : props) {
-            t.replace("(\\b\\B)", " ");
+            System.out.println(m.group());
+            //m.group();
         }
         throw (new ItemParseException());
     }
+
+//        String[] props = singleItem.replace(";", " ").split(" ");
+//        for (String t : props) {
+//            t.replace("("\\W")", " ");
+//        }
+//        throw (new ItemParseException());
+//}
 }

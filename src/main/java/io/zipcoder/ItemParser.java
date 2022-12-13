29814636c260ab85eec69744856ea532b1a34a44
annafu1 <104175541+annafu1@users.noreply.github.com>
2022-12-13 00:12:59 -0500

    finally did it

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -11,18 +11,19 @@ import java.util.List;
 
 public class ItemParser {
     public List<Item> parseItemList(String valueToParse) {
-        try {
-            String[] items = valueToParse.split("##");
-            ArrayList<Item> list = new ArrayList<>();
-            for (String t : items) {
-               list.add(parseSingleItem(t));
+        String[] items = valueToParse.split("##");
+        ArrayList<Item> list = new ArrayList<>();
+        for (String t :items) {
+            try {
+                list.add(parseSingleItem(t));
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
             }
-            return list;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
+        return list;
     }
-        // Pattern pattern = Pattern.compile("milk")
+
+    // Pattern pattern = Pattern.compile("milk")
 //        Matcher matcher = pattern.matcher(valueToParse);
 //        //String[] input = valueToParse.split(",\\s*");
 //        List<Item> list = new ArrayList<>();
@@ -33,21 +34,17 @@ public class ItemParser {
     //return first;
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        Pattern pattern = Pattern.compile("\\w");
+        Pattern pattern = Pattern.compile("\\w+");
         Matcher m = pattern.matcher(singleItem);
         MatchGroup mg = new MatchGroup(m);
-        while(m.find()) {
-
-            System.out.println(m.group());
-            //m.group();
+        try {
+            //need to print format of expected ("milk" 3.23 "food" "1/25/2016")
+                                  //   1  23 4 5   6  7 8 9   10   spaces are 2,5,7
+            Item s = new Item(mg.get(1).getValue().toLowerCase(), Double.parseDouble(mg.get(3).getValue() + "." + mg.get(4).getValue()), mg.get(6).getValue().toLowerCase(), mg.get(8).getValue() + "/" + mg.get(9).getValue() + "/" + mg.get(10).getValue());
+            return s;
+        } catch(Exception exception) {
+            System.out.println(exception.getMessage());
+            throw new ItemParseException();
         }
-        throw (new ItemParseException());
     }
-
-//        String[] props = singleItem.replace(";", " ").split(" ");
-//        for (String t : props) {
-//            t.replace("("\\W")", " ");
-//        }
-//        throw (new ItemParseException());
-//}
 }
